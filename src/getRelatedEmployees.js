91482a831ad314341f1d0eb3df90742cfdb60576
Christian <chrisnebech@hotmail.com>
2022-07-11 01:06:44 -0300

    requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,24 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function isManager(id) {
-  // seu código aqui
+  return employees.some((employee) => employee.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+
+  const workers = [];
+  employees.forEach((employee) => {
+    if (employee.managers.includes(managerId)) {
+      workers.push(`${employee.firstName} ${employee.lastName}`);
+    }
+  });
+
+  return workers;
 }
 
 module.exports = { isManager, getRelatedEmployees };
