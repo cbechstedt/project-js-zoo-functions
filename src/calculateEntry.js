e1cfbb44d9b3e96d6b742b3f3ba0e34b7da13e72
Christian <chrisnebech@hotmail.com>
2022-07-14 18:31:47 -0300

    requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,22 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aquii
+  const obj = {};
+  const children = entrants.filter((person) => person.age < 18).length;
+  const adults = entrants.filter((person) => person.age >= 18 && person.age < 50).length;
+  const seniors = entrants.filter((person) => person.age >= 50).length;
+  obj.child = children;
+  obj.adult = adults;
+  obj.senior = seniors;
+  return obj;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) { return 0; }
+  const visitors = countEntrants(entrants);
+  const sum = (visitors.child * data.prices.child)
+    + (visitors.adult * data.prices.adult) + (visitors.senior * data.prices.senior);
+  return sum;
 }
 
 module.exports = { calculateEntry, countEntrants };
