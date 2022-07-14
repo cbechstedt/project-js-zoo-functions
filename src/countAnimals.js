3a4d436e3fdfb152c5f2cb4aef0c378ef92a9eb2
Christian <chrisnebech@hotmail.com>
2022-07-14 02:13:22 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  const objAnimals = {};
+  if (animal === undefined) {
+    data.species.forEach((element) => {
+      objAnimals[element.name] = element.residents.length;
+    });
+    return objAnimals;
+  }
+
+  if (Object.keys(animal).length === 2) {
+    const findAnimal = data.species.find((element) => element.name === animal.specie);
+    const collectAnimals = findAnimal.residents.filter((element) => element.sex === animal.sex);
+    return collectAnimals.length;
+  }
+
+  const findAnimal = data.species.find((element) => element.name === animal.specie);
+  return findAnimal.residents.length;
 }
 
 module.exports = countAnimals;
