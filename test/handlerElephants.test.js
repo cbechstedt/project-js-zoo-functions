6401e20318e4bb77f084de514c2127a3f992dfb7
Christian <chrisnebech@hotmail.com>
2022-07-15 23:04:37 -0300

    requisitos 6 e 7

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,31 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('é uma função', () => {
+    expect(typeof handlerElephants).toBe('function');
+  });
+
+  it('retorna undefined se função é chamada sem parâmetros', () => {
+    expect(handlerElephants()).toBe(undefined);
+  });
+
+  it('retorna a quantidade 4 se função é chamada com param count', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+
+  it('retorna um array de strings com os nomes se função é chamada com param names', () => {
+    expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
+  });
+
+  it('retorna o número 10.5 se função é chamada com param averageAge', () => {
+    expect(handlerElephants('averageAge')).toEqual(10.5);
+  });
+
+  it('retorna NW se função é chamada com param location', () => {
+    expect(handlerElephants('location')).toEqual('NW');
+  });
+
+  it('retorna um número igual a 5 se a função é chamada com param popularity', () => {
+    expect(handlerElephants('popularity')).toEqual(5);
+  });
+});
