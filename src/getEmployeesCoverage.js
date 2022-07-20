b27454cf989284ffb62fb19fe2d46f31b97fbea7
Christian <chrisnebech@hotmail.com>
2022-07-19 22:51:20 -0300

    requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,40 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { species, employees } = data;
+
+const findPerson = (param) => {
+  if (param.name) {
+    return employees.find(({ firstName, lastName }) => [firstName, lastName].includes(param.name));
+  } if (param.id) {
+    return employees.find(({ id }) => id === param.id);
+  }
+};
+
+const employeesInfos = (param2) => {
+  const infos = findPerson(param2);
+  if (!infos) {
+    throw new Error('Informações inválidas');
+  }
+  const resp = species.filter((element) => infos.responsibleFor.includes(element.id));
+  return {
+    id: infos.id,
+    fullName: `${infos.firstName} ${infos.lastName}`,
+    species: resp.map((element) => element.name),
+    locations: resp.map((element) => element.location),
+  };
+};
+
+const findNames = () => {
+  const array = employees.map((element) => employeesInfos({ id: element.id }));
+  return array;
+};
+console.log(findNames());
+
+function getEmployeesCoverage(param3) {
+  if (!param3) {
+    return findNames();
+  }
+  return employeesInfos(param3);
 }
 
 module.exports = getEmployeesCoverage;
